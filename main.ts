24a75f5ca362eb5141c8966d0e4f60c31cb8e810
Riven <4771215@qq.com>
2019-07-11 19:35:13 +0800

    works with meowbit

--- main.ts
+++ main.ts
@@ -8,13 +8,15 @@ load dependency
 namespace sdwireless {
 
     type EvtStr = (data: string) => void;
+    type EvtBuff = (data: Buffer) => void;
 
     let onMsg: EvtStr;
+    let onMsgBuff: EvtBuff;
     let spi: SPI;
     let cs = pins.P8;
     let irq = pins.P2;
 
-    function spiTx(b: string) {
+    function spiTx(b: Buffer) {
         if (!spi) return;
         let tx = pins.createBuffer(b.length + 4)
         let rx = pins.createBuffer(b.length + 4)
@@ -24,15 +26,15 @@ namespace sdwireless {
         tx.setUint8(2, 0xe0)
         tx.setUint8(3, b.length)
         for (let j = 0; j <= b.length; j++) {
-            tx.setUint8(j + 4, b.charCodeAt(j))
+            tx.setUint8(j + 4, b[j])
         }
         cs.digitalWrite(false)
         spi.transfer(tx, rx)
         cs.digitalWrite(true)
     }
 
-    function spiRx(): string {
-        if (!spi) return 'No SPI';
+    function spiRx(): Buffer {
+        if (!spi) return null;
         // max 24 bytes as microbit ble supported
         let tx = pins.createBuffer(24 + 4)
         let rx = pins.createBuffer(24 + 4)
@@ -45,7 +47,8 @@ namespace sdwireless {
         cs.digitalWrite(false)
         spi.transfer(tx, rx)
         cs.digitalWrite(true)
-        return rx.slice(3).toString()
+        let len = rx[3]
+        return rx.slice(4, len)
     }
 
     //% blockId=sdw_init block="SD wireless init"
@@ -55,22 +58,34 @@ namespace sdwireless {
         spi.setMode(3)
         spi.setFrequency(1000000)
         cs.digitalWrite(true)
-        irq.onPulsed(PulseValue.High, function () {
+
+        irq.onEvent(PinEvent.PulseHigh, function () {
             let msg = spiRx()
-            if (onMsg) onMsg(msg)
+            if (onMsg) onMsg(msg.toString())
+            if (onMsgBuff) onMsgBuff(msg)
         })
     }
 
     //% blockId=sdw_tx block="Send message %data"
     //% weight=90
     export function sdw_tx(data: string): void {
-        spiTx(data)
+        let buf = pins.createBuffer(data.length)
+        for (let i=0;i<data.length;i++){
+            buf.setUint8(i, data.charCodeAt(i))
+        }
+        spiTx(buf)
     }
 
     //% blockId=sdw_ondata block="on Message"
     //% weight=90
     export function sdw_ondata(handler: (sdMsg: string) => void): void {
         onMsg = handler;
+    }
+
+    //% blockId=sdw_ondata_buff block="on Message Buff"
+    //% weight=90
+    export function sdw_ondata_buff(handler: (sdMsg: Buffer) => void): void {
+        onMsgBuff = handler;
     }
 
 }
